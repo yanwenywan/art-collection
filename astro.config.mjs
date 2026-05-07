1951f8c95e804f2d83710ed87c89f140db163e40
yan xin <yan@yanwenyuan.uk>
2026-05-08 00:16:07 +0100

    minor admjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yan.yanwenyuan.uk',
+  site: "https://yan.yanwenyuan.uk",
+  trailingSlash: "always",
 });
