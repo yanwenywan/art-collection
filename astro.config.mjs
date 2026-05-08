1409d07d6367462fb5127d1998234d92560514b6
Yan Xin <yan@yanwenyuan.uk>
2026-05-08 10:26:17 +0100

    add workflow back in

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yan.yanwenyuan.uk",
+  site: "https://art.yanwenyuan.uk",
   trailingSlash: "always",
 });
